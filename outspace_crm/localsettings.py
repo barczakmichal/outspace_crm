3a725d73a1eb04ff551a37f064ff8b72678de30e
barczakmichal <barczakmichal@gmail.com>
2021-03-18 10:24:23 +0100

    first commit

---
+++ outspace_crm/localsettings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'outspace_crm',
+        'USER': 'outspace',
+        'PASSWORD': 'rYnb<yV@\J',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
+    }
+}
